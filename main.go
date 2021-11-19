a70bdf57147b838c23192c134ee32398e70b7bc7
k-p5w <kos.p5w@gmail.com>
2021-11-19 18:22:45 +0900

    portを取得するようにする

--- main.go
+++ main.go
@@ -1,14 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"os"
 
 	persona "github.com/k-p5w/go-persona/api"
 )
 
 func main() {
 	http.HandleFunc("/start", persona.Handler)
-	port := "8080"
+	port := os.Getenv("PORT")
+	fmt.Println(port)
+	if port == "" {
+		port = "9999"
+	}
 	// 公開する
 	http.ListenAndServe(":"+port, nil)
 }
