1b371a96eafdbcf82ce597383c57e51625ddd8df
k-p5w <kos.p5w@gmail.com>
2021-11-19 18:15:09 +0900

    指定してみる

--- main.go
+++ main.go
@@ -2,17 +2,13 @@ package main
 
 import (
 	"net/http"
-	"os"
 
 	persona "github.com/k-p5w/go-persona/api"
 )
 
 func main() {
-	http.HandleFunc("/", persona.Handler)
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "9999"
-	}
+	http.HandleFunc("/start", persona.Handler)
+	port := "8080"
 	// 公開する
 	http.ListenAndServe(":"+port, nil)
 }
