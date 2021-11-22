e6cfff867ca01a3a29898756fd4ff8b0c4793315
k-p5w <kos.p5w@gmail.com>
2021-11-22 18:23:55 +0900

    404ページ対応

--- api/index.go
+++ api/index.go
@@ -8,3 +8,10 @@ import (
 func MyHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
 }
+
+func errorHandler(w http.ResponseWriter, r *http.Request, status int) {
+	w.WriteHeader(status)
+	if status == http.StatusNotFound {
+		fmt.Fprint(w, "custom 404")
+	}
+}
--- main.go
+++ main.go
@@ -13,6 +13,8 @@ func main() {
 	http.Handle("/", http.FileServer(http.Dir("static")))
 
 	http.HandleFunc("/start", persona.MyHandler)
+	http.HandleFunc("/home", homeHandler)
+	http.HandleFunc("/eg", smthHandler)
 	port := os.Getenv("PORT")
 	fmt.Println(port)
 	if port == "" {
@@ -20,4 +22,27 @@ func main() {
 	}
 	// 公開する
 	http.ListenAndServe(":"+port, nil)
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		errorHandler(w, r, http.StatusNotFound)
+		return
+	}
+	fmt.Fprint(w, "welcome home")
+}
+
+func smthHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/smth/" {
+		errorHandler(w, r, http.StatusNotFound)
+		return
+	}
+	fmt.Fprint(w, "welcome smth")
+}
+
+func errorHandler(w http.ResponseWriter, r *http.Request, status int) {
+	w.WriteHeader(status)
+	if status == http.StatusNotFound {
+		fmt.Fprint(w, "custom 404")
+	}
 }
