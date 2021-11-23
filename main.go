ba558d50c574a943f494a991878649613d734ec3
k-p5w <kos.p5w@gmail.com>
2021-11-23 11:50:11 +0900

    serverless functionのテスト

--- main.go
+++ main.go
@@ -2,26 +2,55 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
-	"os"
-
-	persona "github.com/k-p5w/go-persona/api"
+	"time"
 )
 
-func main() {
+// func main() {
 
-	http.Handle("/", http.FileServer(http.Dir("static")))
+// 	// vercel だとgo runされないっぽいな、wasm的な感じをつくればいいのかなぁ
+// 	http.Handle("/", http.FileServer(http.Dir("static")))
 
-	http.HandleFunc("/start", persona.MyHandler)
-	http.HandleFunc("/home", homeHandler)
-	http.HandleFunc("/eg", smthHandler)
-	port := os.Getenv("PORT")
-	fmt.Println(port)
-	if port == "" {
-		port = "9999"
+// 	http.HandleFunc("/start", persona.MyHandler)
+// 	http.HandleFunc("/home", homeHandler)
+// 	http.HandleFunc("/eg", smthHandler)
+// 	port := os.Getenv("PORT")
+// 	fmt.Println(port)
+// 	if port == "" {
+// 		port = "9999"
+// 	}
+// 	// 公開する
+// 	http.ListenAndServe(":"+port, nil)
+// }
+
+func addServedHeader(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("X-Served-Date", time.Now().String())
+}
+
+func makeRequestHandler(middleware http.HandlerFunc) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		middleware(w, r)
+
+		w.Write([]byte("OK"))
 	}
-	// 公開する
-	http.ListenAndServe(":"+port, nil)
+}
+
+func main() {
+	s := &http.Server{
+		Addr:           fmt.Sprintf(":%d", 8080),
+		ReadTimeout:    3 * time.Second,
+		WriteTimeout:   3 * time.Second,
+		MaxHeaderBytes: 1 << 20, // Max header of 1MB
+	}
+
+	next := addServedHeader
+	http.HandleFunc("/", makeRequestHandler(next))
+	log.Fatal(s.ListenAndServe())
+}
+
+func Handle(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
 }
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
